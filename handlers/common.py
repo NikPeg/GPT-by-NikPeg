e03ca008de7e67a26016f6912602e23f35553ec7
NikPeg <peganov.nik@gmail.com>
2024-05-14 09:32:52 +0300

    parsing in send_big_message

--- handlers/common.py
+++ handlers/common.py
@@ -16,34 +16,18 @@ async def create_user_req(user_id, user_name, request_text):
         await bot.send_chat_action(user_id, TYPING_ACTION)
 
     await typing()
-    try:
-        await send_big_message(
-            bot,
-            ADMIN_ID,
-            messages.MESSAGE_SENT.format(user_id, user_name, request_text),
-            parse_mode=None,
-        )
-    except Exception as e:
-        await send_big_message(
-            bot,
-            ADMIN_ID,
-            messages.PARSING_ERROR.format(e) + "line 30",
-        )
+    await send_big_message(
+        bot,
+        ADMIN_ID,
+        messages.MESSAGE_SENT.format(user_id, user_name, request_text),
+        parse_mode=None,
+    )
     thread_id = get_thread_id(user_id)
     await typing()
     await gpt.add_message(thread_id, request_text)
     await typing()
     bot_answer = await gpt.get_answer(thread_id, typing)
-    for parse_mode in [ParseMode.HTML, ParseMode.MARKDOWN_V2]:
-        try:
-            await send_big_message(bot, user_id, markdown_to_html(bot_answer), parse_mode=parse_mode)
-            break
-        except Exception as e:
-            await send_big_message(
-                bot,
-                ADMIN_ID,
-                messages.PARSING_ERROR.format(e) + "line 45",
-            )
+    await send_big_message(bot, user_id, markdown_to_html(bot_answer))
     await send_big_message(
         bot,
         ADMIN_ID,
--- utils/bot_utils.py
+++ utils/bot_utils.py
@@ -1,6 +1,23 @@
+from aiogram.types import ParseMode
+
+import messages
+from config import ADMIN_ID
+
 MAX_MESSAGE_LENGTH = 4096
 
 
-async def send_big_message(bot, user_id, text, *args, **kwargs):
+async def send_big_message(bot, user_id, text):
     for i in range(0, len(text), MAX_MESSAGE_LENGTH):
-        await bot.send_message(user_id, text[i:i + MAX_MESSAGE_LENGTH], *args, **kwargs)
+        err = None
+        for parse_mode in [ParseMode.HTML, ParseMode.MARKDOWN_V2, ParseMode.MARKDOWN]:
+            try:
+                await bot.send_message(user_id, text[i:i + MAX_MESSAGE_LENGTH], parse_mode=parse_mode)
+                break
+            except Exception as e:
+                err = e
+        else:
+            await send_big_message(
+                bot,
+                ADMIN_ID,
+                messages.PARSING_ERROR.format(err),
+            )
