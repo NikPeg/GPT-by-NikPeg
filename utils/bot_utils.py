0110df49099ef73d705f7cdec2a87353205f15dc
NikPeg <peganov.nik@gmail.com>
2024-05-14 10:57:05 +0300

    print errors

--- utils/bot_utils.py
+++ utils/bot_utils.py
@@ -16,15 +16,15 @@ async def send_big_message(bot, user_id, text):
             continue
         except Exception as e:
             await bot.send_message(ADMIN_ID, e)
+            print(e)
+            err = e
         for parse_mode in [ParseMode.MARKDOWN_V2, ParseMode.MARKDOWN, ParseMode.HTML, None]:
             try:
                 await bot.send_message(user_id, text[i:i + MAX_MESSAGE_LENGTH], parse_mode=parse_mode)
                 break
             except Exception as e:
                 await bot.send_message(ADMIN_ID, e)
+                print(e)
+                err = e
         else:
-            await send_big_message(
-                bot,
-                ADMIN_ID,
-                messages.PARSING_ERROR.format(err),
-            )
+            bot.send_message(ADMIN_ID, messages.PARSING_ERROR.format(err))
