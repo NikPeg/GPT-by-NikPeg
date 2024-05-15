d7f0505599416c05cb65a0a20b4fe80b14e7ef58
NikPeg <peganov.nik@gmail.com>
2024-05-15 10:32:19 +0300

    . too

--- utils/formatting.py
+++ utils/formatting.py
@@ -1,32 +1,6 @@
 import re
 
 
-# def screen_symbols(s):
-#     s = re.sub(r'\@', '\@', s)
-#     s = re.sub(r'\.', '\.', s)
-#     s = re.sub(r'\!', '\!', s)
-#     s = re.sub(r'\#', '\#', s)
-#     s = re.sub(r'\$', '\$', s)
-#     s = re.sub(r'\%', '\%', s)
-#     s = re.sub(r'\&', '\&', s)
-#     s = re.sub(r'\-', '\-', s)
-#     s = re.sub(r'\(', '\(', s)
-#     s = re.sub(r'\)', '\)', s)
-#     s = re.sub(r'\{', '\{', s)
-#     s = re.sub(r'\}', '\}', s)
-#     s = re.sub(r'\[', '\[', s)
-#     s = re.sub(r'\]', '\]', s)
-#     s = re.sub(r'\\', '\\\\', s)
-#     s = re.sub(r'\+', '\+', s)
-#     s = re.sub(r'\=', '\=', s)
-#     s = re.sub(r'\>', '\>', s)
-#     s = re.sub(r'\<', '\<', s)
-#     s = re.sub(r'\|', '\|', s)
-#     s = re.sub(r'\_', '\_', s)
-#     s = re.sub(r'\`', '\`', s)
-#     return s
-
-
 def screen_symbols(s):
     s = re.escape(s)
     s = re.sub(r'\!', '\!', s)
