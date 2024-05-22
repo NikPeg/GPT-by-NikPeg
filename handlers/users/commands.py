e71576ba3e80b6afaace698432d613ba1ec55515
NikPeg <peganov.nik@gmail.com>
2024-05-22 23:41:22 +0300

    delete try

--- handlers/users/commands.py
+++ handlers/users/commands.py
@@ -204,10 +204,10 @@ async def user_gpt_req_handler(message: types.Message):
             files.append(photo_file)
         print("photos", files)
 
-    try:
-        await asyncio.create_task(create_user_req(message.chat.id, message.chat.username, request_text, files or None))
-    except openai.BadRequestError as e:
-        await bot.send_message(message.chat.id, messages.WAIT)
-        await bot.send_message(ADMIN_ID, messages.WAIT + e)
-    except Exception as e:
-        await bot.send_message(ADMIN_ID, messages.UNKNOWN_ERROR.format(str(e)))
+    # try:
+    await asyncio.create_task(create_user_req(message.chat.id, message.chat.username, request_text, files or None))
+    # except openai.BadRequestError as e:
+    #     await bot.send_message(message.chat.id, messages.WAIT)
+    #     await bot.send_message(ADMIN_ID, messages.WAIT + e)
+    # except Exception as e:
+    #     await bot.send_message(ADMIN_ID, messages.UNKNOWN_ERROR.format(str(e.__traceback__)))
