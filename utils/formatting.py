cfd06c2ce533880533ce88beded091c13b68ee2f
NikPeg <peganov.nik@gmail.com>
2024-05-25 23:00:58 +0300

    Normal headers escaping

--- utils/formatting.py
+++ utils/formatting.py
@@ -12,14 +12,14 @@ def escape_symbols(s):
     return s
 
 
-def escape_markdown_symbols(s):
+def escape_headers(s, open_symbol="*", close_symbol="*"):
     # Заменяем заголовки
-    s = re.sub(r"^### (.*?)\n", r"*\1*\n", s)
-    s = re.sub(r"\n### (.*?)\n", r"\n*\1*\n", s)
-    s = re.sub(r"\n### (.*?)\n", r"\n*\1*\n", s)
-    s = re.sub(r"\n### (.*?)$", r"\n*\1*", s)
-    s = re.sub(r"^### (.*?)$", r"\n*\1*", s)
+    pattern = re.compile(r'^(#{2,6})\s+(.*)', re.MULTILINE)
+    return re.sub(pattern, lambda match: f"{open_symbol}{match.group(2)}{close_symbol}", s)
 
+
+def escape_markdown_symbols(s):
+    s = escape_headers(s)
     characters_to_escape = set("!@#$%^&()+-={}\\[]./|<>")
     escaped_string = ''.join(['\\' + char if char in characters_to_escape else char for char in s])
     return escaped_string
@@ -32,10 +32,6 @@ def markdown_to_html(s):
     s = re.sub(r"\*(.*?)\*", r"<i>\1</i>", s)
 
     # Заменяем заголовки
-    s = re.sub(r"^### (.*?)\n", r"<b>\1</b>\n", s)
-    s = re.sub(r"\n### (.*?)\n", r"\n<b>\1</b>\n", s)
-    s = re.sub(r"\n### (.*?)\n", r"\n<b>\1</b>\n", s)
-    s = re.sub(r"\n### (.*?)$", r"\n<b>\1</b>", s)
-    s = re.sub(r"^### (.*?)$", r"\n<b>\1</b>", s)
+    s = escape_headers(s, "<b>", "</b>")
 
     return s
